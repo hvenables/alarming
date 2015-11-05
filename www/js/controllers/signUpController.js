dda59848246c156db2e321244e8831c0bfb04e0f
chweeks <chrisharryweeks@gmail.com>
2015-11-05 17:44:14 +0000

    edited error messages

--- www/js/controllers/signUpController.js
+++ www/js/controllers/signUpController.js
@@ -20,16 +20,7 @@ ionicApp.controller('SignUpCtrl', function($state, $firebaseAuth) {
     return function (error, userData) {
       console.log(userData.uid);
       if (error) {
-        switch (error.code) {
-          case "EMAIL_TAKEN":
-            console.log("The new user account cannot be created because the email is already in use.");
-            break;
-          case "INVALID_EMAIL":
-            console.log("The specified email is not a valid email.");
-            break;
-          default:
-            console.log("Error creating user:", error);
-        }
+        console.log("Error creating user:", error);
       }
       else {
         console.log("Successfully created user account with uid:", userData.uid);
