b6a51666079f63e8e370a07f43da237dd1acafe0
chweeks <chrisharryweeks@gmail.com>
2015-11-05 16:55:17 +0000

    edited myeventsController

--- www/js/controllers/myEventsController.js
+++ www/js/controllers/myEventsController.js
@@ -3,11 +3,7 @@ ionicApp.controller('MyEventsController', function($firebase) {
   var self = this;
 
   var ref = new Firebase('https://event-alarm.firebaseio.com/events');
-  ref.on('value', function(events) {
-    self.eventsArray = [];
-    self.events = events.val();
-    for(var key in self.events){
-      self.eventsArray.push(self.events[key]);
-    }
+  ref.on('value', function(eventsObject) {
+    self.events = eventsObject.exportVal();
   })
 });
