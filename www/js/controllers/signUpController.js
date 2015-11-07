7d6fe8f65c03738f5addf01e53f23b8a41c9171c
Anindya Bhattacharyya <bat020@gmail.com>
2015-11-07 01:48:04 +0000

    second refactor of SignUpCtrl

--- www/js/controllers/signUpController.js
+++ www/js/controllers/signUpController.js
@@ -6,23 +6,17 @@ ionicApp.controller('SignUpController', function($state, $firebaseAuth) {
   self.signUp = function (user) {
     $firebaseAuth(ref).$createUser(user).then(function (userData) {
       saveNewUser(userData.uid, user.email);
-      logIn(user);
-    }).catch(function (error) {
-      alert(error);
-    });
-  };
-
-  function saveNewUser(userId, userEmail) {
-    ref.child('users/'+userId).set({
-      email: userEmail
-    });
-  };
-
-  function logIn(user) {
-    $firebaseAuth(ref).$authWithPassword(user).then(function () {
+      return $firebaseAuth(ref).$authWithPassword(user);
+    }).then(function () {
       $state.go('tabs.myEvents');
     }).catch(function (error) {
       alert(error);
+    });
+  };
+
+  function saveNewUser(uid, email) {
+    ref.child('users/' + uid).set({
+      email: email
     });
   };
 
