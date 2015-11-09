f3bbe3126409d81af3c45067c894fc164f10af3b
chweeks <chrisharryweeks@gmail.com>
2015-11-09 17:25:06 +0000

    added onAuth locla notifications

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@ var ionicApp = angular.module('alarming', ['ionic', 'ngCordova', 'firebase']);
 
 var self = this;
 
-ionicApp.run(function($ionicPlatform, $cordovaLocalNotification, $interval, $cordovaGeolocation) {
+ionicApp.run(function($ionicPlatform, $cordovaLocalNotification, $interval, $cordovaGeolocation, $http, $firebaseObject) {
   $ionicPlatform.ready(function() {
 
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -13,15 +13,13 @@ ionicApp.run(function($ionicPlatform, $cordovaLocalNotification, $interval, $cor
       StatusBar.styleDefault();
     }
 
-    var ref = new Firebase('https://event-alarm.firebaseio.com/');
-    ref.child('events').on('value', function(events) {
-      self.events = events.val();
-      for (var key in self.events) {
-        notification(self.events[key]);
-      }
-    });
 
-    function notification(currentEvent) {
+    var ref = new Firebase('https://event-alarm.firebaseio.com/');
+    var usersRef = new Firebase('https://event-alarm.firebaseio.com/users');
+    var eventsRef = new Firebase('https://event-alarm.firebaseio.com/events');
+    var currentUserId = usersRef.getAuth().uid;
+
+    self.notification = function (currentEvent) {
       $cordovaLocalNotification.schedule({
         id: currentEvent.id,
         title: currentEvent.eventTitle,
@@ -29,6 +27,32 @@ ionicApp.run(function($ionicPlatform, $cordovaLocalNotification, $interval, $cor
         at: Date.parse(currentEvent.dateTime),
       })
     }
+
+    ref.child('events').on('value', function(events) {
+      self.events = events.val();
+      for (var key in self.events) {
+        notification(self.events[key]);
+      }
+    });
+
+    ref.onAuth(function() {
+      currentUserId = usersRef.getAuth().uid;
+      $http({
+        method: 'GET',
+        url: 'https://event-alarm.firebaseio.com/users/'+currentUserId+'.json'
+      }).then(function successCallback(response) {
+        var users = angular.fromJson(response.data);
+        for(key in users.events){
+          $http({
+            method: 'GET',
+            url: 'https://event-alarm.firebaseio.com/events/'+key+'.json'
+          }).then(function successCallback(response) {
+            var events = angular.fromJson(response.data);
+            self.notification(events);
+          })
+        }
+      });
+    });
   });
 });
 
