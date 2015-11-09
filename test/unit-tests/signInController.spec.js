43f46ec94411bcf204610b84d65e20f7f16b9024
Matt Paul <mattpaul001@gmail.com>
2015-11-09 16:40:53 +0000

    Sign in unit test passing

--- test/unit-tests/signInController.spec.js
+++ test/unit-tests/signInController.spec.js
@@ -3,20 +3,22 @@ describe('SignInController', function() {
   beforeEach (angular.mock.module('alarming'));
 
   var ctrl, $scope;
+  var authService;
+  var q;
+  // var MockFirebase = require('mockfirebase').MockFirebase;
 
-  var MockFirebase = require('mockfirebase').MockFirebase;
 
-
-  beforeEach(inject(function($controller, $rootScope) {
+  beforeEach(inject(function($controller, $rootScope, $q) {
     scope = $rootScope.$new();
-    ctrl = $controller('SignInController', { $scope:$scope });
-    authService = jasmine.spyObj('authService', ['login']);
+    authService = jasmine.createSpyObj('authService', ['logIn']);
+    q = $q;
+    ctrl = $controller('SignInController', { authService: authService });
+    authService.logIn.and.returnValue(q.when({"Just need to return object":"from the promise, this can be anything"}));
   }));
 
-
-   // it ('calls the auth service', function() {
-   // ctrl.signIn("Bat");
-   // expect(user).toBe('Bat');
-   // });
+   it ('calls the auth service', function() {
+     ctrl.signIn("Bat@bat.com");
+     expect(authService.logIn).toHaveBeenCalled();
+   });
 
 });
