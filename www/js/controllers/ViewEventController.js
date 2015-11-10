d727d33664d4963b01f981c1eec929fb739abeb3
chweeks <chrisharryweeks@gmail.com>
2015-11-10 19:35:33 +0000

    notification routing

--- www/js/controllers/ViewEventController.js
+++ www/js/controllers/ViewEventController.js
@@ -1,6 +1,6 @@
 ionicApp.controller('ViewEventController', ViewEventController);
 
-function ViewEventController(UserService) {
+function ViewEventController(UserService, $location) {
 
   this.userEvent = UserService.userEvents[window.location.hash.slice(17)];
 
--- www/js/controllers/createEventController.js
+++ www/js/controllers/createEventController.js
@@ -1,4 +1,4 @@
-ionicApp.controller('CreateEventController', function($cordovaLocalNotification, $state, $firebaseObject) {
+ionicApp.controller('CreateEventController', function($state, $firebaseObject) {
 
   var self = this;
 
@@ -7,7 +7,7 @@ ionicApp.controller('CreateEventController', function($cordovaLocalNotification,
 
   self.models = {};
   self.attendeeArray = [];
-  //self.usersHash = $firebaseObject(usersRef);
+  self.usersHash = $firebaseObject(usersRef);
 
   eventsRef.on('child_added', function (snapshot) {
     var ownerRef = usersRef.child(snapshot.val().owner);
