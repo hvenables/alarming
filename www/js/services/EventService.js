b19ea16af52e3522205bc8865962a2af5c77c3fd
Anindya Bhattacharyya <bat020@gmail.com>
2015-11-10 12:22:16 +0000

    renamed controllers

--- www/js/services/EventService.js
+++ www/js/services/EventService.js
@@ -1,3 +1,11 @@
 ionicApp.service('EventService', EventSeObjecte);
 
-function EventService($firebaseObject) {}
+function EventService($firebaseObject) {
+
+  var ref = new Firebase('https://event-alarm.firebaseio.com/');
+  var eventsRef = ref.child('events');
+  var usersRef = ref.child('users');
+
+
+
+}
