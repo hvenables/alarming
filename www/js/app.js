82c34cb1f6bf1175f1e8cfcdf01bb6703ac85936
chweeks <chrisharryweeks@gmail.com>
2015-11-11 13:56:35 +0000

    removed filter

--- www/js/app.js
+++ www/js/app.js
@@ -58,21 +58,6 @@ ionicApp.run(function($ionicPlatform, $cordovaLocalNotification, $interval, $cor
   });
 });
 
-ionicApp.filter('orderObjectBy', function() {
-  return function(items, field, reverse) {
-    var filtered = [];
-    angular.forEach(items, function(item) {
-      filtered.push(item);
-    });
-    filtered.sort(function (a, b) {
-      return (a[field] > b[field] ? 1 : -1);
-    });
-    if(reverse) filtered.reverse();
-    return filtered;
-  };
-});
-
-
 ionicApp.config(function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/');
