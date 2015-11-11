b1d9cb288a8aed368cdecc0a48a6ed7254e42172
chweeks <chrisharryweeks@gmail.com>
2015-11-11 13:43:20 +0000

    renamed the create event controller

--- www/js/controllers/CreateEventController.js
+++ www/js/controllers/CreateEventController.js
@@ -1,6 +1,6 @@
 ionicApp.controller('CreateEventController', CreateEventController);
 
-function CreateEventController(EventService, $state) {
+function CreateEventController(EventService, $state, $http) {
 
   this.addToAttendeeHash = function (key, email) {
     EventService.addToAttendeeHash(key, email);
@@ -8,9 +8,17 @@ function CreateEventController(EventService, $state) {
 
   this.usersHash = EventService.usersHash;
 
-  this.createEvent = function (title, description, date, time) {
-    EventService.createEventHash(title, description, date, time);
-    $state.go('tabs.myEvents')
+  this.createEvent = function (title, description, date, time, postcode) {
+    var eventPostcode = postcode.replace(/\s+/g, '');
+    var url = 'http://api.postcodes.io/postcodes/' + eventPostcode;
+    $http.get(url).success(function (data, status, headers, config) {
+      latlong = {
+        lat: data.result.latitude,
+        lng: data.result.longitude
+      };
+      EventService.createEventHash(title, description, date, time, postcode, latlong);
+      $state.go('tabs.myEvents');
+    });
   };
 
 }
